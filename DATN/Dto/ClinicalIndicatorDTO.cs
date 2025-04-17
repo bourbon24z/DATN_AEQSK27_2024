dcce034efeb0373aabb75075394495854037c27b
Silsas120703 <tuvo1207003@gmail.com>
2025-04-17 15:32:49 +0700

    addIndicationController

---
+++ DATN/Dto/ClinicalIndicatorDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DATN.Dto
+{
+	public class ClinicalIndicatorDTO
+	{
+		public int UserID { get; set; }
+		public DateTime RecordedAt { get; set; }
+		public bool DauDau { get; set; }
+		public bool TeMatChi { get; set; }
+		public bool ChongMat { get; set; }
+		public bool KhoNoi { get; set; }
+		public bool MatTriNhoTamThoi { get; set; }
+		public bool LuLan { get; set; }
+		public bool GiamThiLuc { get; set; }
+		public bool MatThangCan { get; set; }
+		public bool BuonNon { get; set; }
+		public bool KhoNuot { get; set; }
+	}
+}
---
+++ DATN/Dto/MolecularIndicatorDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace DATN.Dto
+{
+	public class MolecularIndicatorDTO
+	{
+		public int UserID { get; set; }
+		public DateTime RecordedAt { get; set; }
+		public bool MiR_30e_5p { get; set; }
+		public bool MiR_16_5p { get; set; }
+		public bool MiR_140_3p { get; set; }
+		public bool MiR_320d { get; set; }
+		public bool MiR_320p { get; set; }
+		public bool MiR_20a_5p { get; set; }
+		public bool MiR_26b_5p { get; set; }
+		public bool MiR_19b_5p { get; set; }
+		public bool MiR_874_5p { get; set; }
+		public bool MiR_451a { get; set; }
+	}
+}
