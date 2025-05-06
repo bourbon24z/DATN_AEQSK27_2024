139ff3c552f2c3ac33e4a40d700c64847d5a0e89
HungLeeeee <ltqh051203@gmail.com>
2025-05-06 23:55:35 +0700

    add SystemHealthyContrller

---
+++ DATN/Controllers/SystemHealthsController.cs
@@ -0,0 +1,36 @@
+﻿using DATN.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DATN.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemHealthsController : ControllerBase
+    {
+        private readonly StrokeDbContext _context;
+        public SystemHealthsController(StrokeDbContext context)
+        {
+            _context = context;
+        }
+
+       // [Authorize(Roles ="admin")]  test tis thoii cos chi merge laij nha Huy 
+        [HttpGet("CountUser")]
+        public async Task<IActionResult> GetCountUser()
+        {
+            int countUser = await _context.StrokeUsers.CountAsync();
+            if (countUser > 0)
+            {
+                return Ok(new { countUser });
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+    }
+
+}
